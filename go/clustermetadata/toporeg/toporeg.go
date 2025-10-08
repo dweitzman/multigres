2f0a09e051a1e17d7b78a7e3ef6edade1d86dc95
David Weitzman <100598+dweitzman@users.noreply.github.com>
2025-10-08 10:30:29 -0400

    Skip first attempt
    Signed-off-by: David Weitzman <100598+dweitzman@users.noreply.github.com>

--- go/clustermetadata/toporeg/toporeg.go
+++ go/clustermetadata/toporeg/toporeg.go
@@ -61,7 +61,8 @@ func Register(register func(ctx context.Context) error, unregister func(ctx cont
 		r := backoff.NewWithOptions(
 			backoff.WithMinDelay(10*time.Millisecond),
 			backoff.WithMaxDelay(30*time.Second),
-			backoff.WithMaxAttempts(0), // Unlimited retries
+			backoff.WithMaxAttempts(0),     // Unlimited retries
+			backoff.WithSkipFirstAttempt(), // We already tried once
 		)
 
 		err := r.Do(tp.ctx, func(attempt int) error {
